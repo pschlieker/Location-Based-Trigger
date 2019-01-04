be25ece8775b44165c93025f92927e358e14a535
Philipp Schlieker <philipp@schliekers.de>
2019-01-04 23:16:48 +0100

    Remove TLM Load from Beacon Advertisement

--- RaspberryPi/beacon.js
+++ RaspberryPi/beacon.js
@@ -6,8 +6,8 @@ var instanceId = '3dfe4b5e89b9';
 var options = {
     name: 'DoorPi', // set device name when advertising (Linux only)
     txPowerLevel: -22, // override TX Power Level, default value is -21,
-    tlmCount: 2, // 2 TLM frames
-    tlmPeriod: 10 // every 10 advertisements
+//    tlmCount: 2, // 2 TLM frames
+//    tlmPeriod: 10 // every 10 advertisements
 };
 
 exports.startBeacon = function () {
